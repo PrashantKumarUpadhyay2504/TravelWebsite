e262cf392cab0dfff737e94266f5ac00c2621dee
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-08-12 17:11:26 +0530

    commit

--- BookingFormCRUD-main/BookingFormCRUD-main/data.php
+++ BookingFormCRUD-main/BookingFormCRUD-main/data.php
@@ -1,8 +1,31 @@
 <?php
-$bname= $_POST['bname'];
-$dname=$_POST['dname'];
-$qty=$_POST['qty'];
-$Price=$_POST['Price'];
+
+//Establishing a connection with PHP 
+$connection_details = mysqli_connect("localhost","root","","travelproject");
+if(!$connection_details)
+{
+	die("problem in connection establish");
+}
+
+// save button coding
+if(isset($_POST['submit']))
+{
+$bookname = $_POST['bookname'];
+$destname = $_POST['destname'];
+$qty = $_POST['qty'];
+$days = $_POST['Days'];
+
+$insert_query = "INSERT INTO book(bookname,destname,qty,Days) VALUES('$bookname','$destname','$qty','$days')";
+if(mysqli_query($connection_details,$insert_query))
+{
+	//echo "data inserted successfully";
+	echo "<script>alert('data is inserted')</script>";
+	header("refresh:0,url=form1.html");
+}
+else{
+	echo "unable to store data";
+}
+}
 
 
 ?>
