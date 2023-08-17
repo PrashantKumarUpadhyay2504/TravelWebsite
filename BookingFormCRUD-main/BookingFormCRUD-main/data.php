cf6bd8d87e2b4ae38e634919f0f19ba436bcd481
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-08-17 12:27:32 +0530

    Update data.php

--- BookingFormCRUD-main/BookingFormCRUD-main/data.php
+++ BookingFormCRUD-main/BookingFormCRUD-main/data.php
@@ -1,8 +1,26 @@
 <?php
-$bname= $_POST['bname'];
+
+$bname=$_POST['bname'];
 $dname=$_POST['dname'];
 $qty=$_POST['qty'];
-$Price=$_POST['Price'];
+$No_Day=$_POST['No_Days'];
 
+$conn=mysqli_connect("localhost","root","","travel_booking_data");
+
+if(!$conn)
+{
+    die("Not connected");
+}
+
+if(isset($_POST['submit'])){
+    $insert_query="INSERT INTO booking(bname,dname,qty,No_Days) VALUES('$bname','$dname','$qty','$No_Day')";
+
+    if(mysqli_query($conn,$insert_query)){
+       header("refresh:0,url=form.html");
+    }
+    else{
+        echo"Not Inserted.........";
+    }
+}
 
 ?>
